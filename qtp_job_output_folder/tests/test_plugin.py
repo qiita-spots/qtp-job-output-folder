f604e26caa802c13c26783c069c0a0984c094613
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-11-13 08:28:15 +0100

    leaves clean test dirs

--- qtp_job_output_folder/tests/test_plugin.py
+++ qtp_job_output_folder/tests/test_plugin.py
@@ -23,10 +23,19 @@ from qtp_job_output_folder import plugin, __version__
 class PluginTests(PluginTestCase):
     def setUp(self):
         self.out_dir = mkdtemp()
-        self.source_dir = join(mkdtemp(), 'test_data')
+        # need to refer to a valid mountpoint (here: job) and a directory
+        # that exists in Qiita DB as 'directory' as fetching via https will
+        # fail otherwise. To avoid collisions, a mid-level directory name is
+        # a random string (= _get_candidate_names())
+        self.mountpoint = 'job'
+        # only adapt filepaths
+        self.source_dir = self.deposite_in_qiita_basedir(
+            join(self.mountpoint, '2_test_folder'), True)
         source = join(dirname(abspath(getfile(currentframe()))), 'test_data')
         copytree(source, self.source_dir)
-        self._clean_up_files = [self.out_dir]
+        self.qclient.push_file_to_central(self.source_dir)
+        self._clean_up_files = [self.out_dir, dirname(self.source_dir)]
+        self._clean_up_remote_files = []
 
     def tearDown(self):
         for fp in self._clean_up_files:
@@ -35,6 +44,8 @@ class PluginTests(PluginTestCase):
                     rmtree(fp)
                 else:
                     remove(fp)
+        for fp in self._clean_up_remote_files:
+            self.qclient.delete_file_from_central(fp)
 
     def _wait_job(self, job_id):
         for i in range(20):
@@ -47,7 +58,8 @@ class PluginTests(PluginTestCase):
     def test_plugin_summary(self):
         # creating new artifact
         files = [(self.source_dir, 'directory')]
-        data = {'filepaths': dumps(files), 'type': 'job-output-folder',
+        atype = 'job-output-folder'
+        data = {'filepaths': dumps(files), 'type': atype,
                 'name': "A name", 'data_type': 'Job Output Folder'}
         aid = self.qclient.post('/apitest/artifact/', data=data)['artifact']
         data = {'command': dumps(['qtp-job-output-folder', __version__,
@@ -56,7 +68,11 @@ class PluginTests(PluginTestCase):
                 'status': 'running'}
         job_id = self.qclient.post(
             '/apitest/processing_job/', data=data)['job']
-        plugin("https://localhost:21174", job_id, self.out_dir)
+        plugin("https://tinqiita-qiita-1:21174", job_id, self.out_dir)
+        fp_target_dir = '%s/%s' % (self.source_dir.split(
+            '/%s/' % self.mountpoint)[0], atype)
+        self._clean_up_remote_files.append(fp_target_dir)
+        self._clean_up_files.append(fp_target_dir)
         self._wait_job(job_id)
         obs = self.qclient.get_job_info(job_id)
         self.assertEqual(obs['status'], 'success')
@@ -64,10 +80,11 @@ class PluginTests(PluginTestCase):
     def test_plugin_validate(self):
         # test success
         files = {'directory': [self.source_dir]}
+        atype = 'job-output-folder'
         parameters = {'template': None,
                       'analysis': None,
                       'files': dumps(files),
-                      'artifact_type': 'job-output-folder'}
+                      'artifact_type': atype}
         data = {
             'command': dumps(
                 ['qtp-job-output-folder', __version__, 'Validate']),
@@ -75,7 +92,11 @@ class PluginTests(PluginTestCase):
             'status': 'running'}
         job_id = self.qclient.post(
             '/apitest/processing_job/', data=data)['job']
-        plugin("https://localhost:21174", job_id, self.out_dir)
+        plugin("https://tinqiita-qiita-1:21174", job_id, self.out_dir)
+        fp_target_dir = '%s/%s' % (self.source_dir.split(
+            '/%s/' % self.mountpoint)[0], atype)
+        self._clean_up_remote_files.append(fp_target_dir)
+        self._clean_up_files.append(fp_target_dir)
         self._wait_job(job_id)
         obs = self.qclient.get_job_info(job_id)
         self.assertEqual(obs['status'], 'success')
@@ -86,7 +107,7 @@ class PluginTests(PluginTestCase):
         data['parameters'] = dumps(parameters)
         job_id = self.qclient.post(
             '/apitest/processing_job/', data=data)['job']
-        plugin("https://localhost:21174", job_id, self.out_dir)
+        plugin("https://tinqiita-qiita-1:21174", job_id, self.out_dir)
         self._wait_job(job_id)
         obs = self.qclient.get_job_info(job_id)
         self.assertEqual(obs['status'], 'error')
