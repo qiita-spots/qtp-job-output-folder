caa23b434ea73c1618d5e87593c72e3f848edd44
Antonio Gonzalez <antgonza@gmail.com>
2025-12-05 14:17:01 -0700

    replace flake8 for ruff

--- qtp_job_output_folder/summary.py
+++ qtp_job_output_folder/summary.py
@@ -6,36 +6,38 @@
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
-from os.path import exists, isdir, join, dirname
 from glob import glob
 from json import dumps
+from os.path import dirname, exists, isdir, join
 
 
 def _folder_listing(folder):
     results = []
-    for f in glob(f'{folder}/*'):
+    for f in glob(f"{folder}/*"):
         if isdir(f):
-            results.append(('folder', f))
-            results.extend(_folder_listing(f'{f}/*'))
+            results.append(("folder", f))
+            results.extend(_folder_listing(f"{f}/*"))
         else:
-            results.append(('file', f))
+            results.append(("file", f))
     return results
 
 
 def _generate_html_summary(jid, folder, out_dir):
-    summary = f'<h3><b>{folder}</b> does not exist.</h3>'
+    summary = f"<h3><b>{folder}</b> does not exist.</h3>"
 
     if exists(folder) and isdir(folder):
         # calculating the "trimming" for the fullpaths, +1 is to remove /
         tname = len(dirname(folder)) + 1
         tlink = len(dirname(dirname(folder)))
-        summary = '<br/>\n'.join([
-            f'<a href=".{f[tlink:]}" type="{ft}" target="_blank">'
-            f'{f[tname:]}</a>'
-            for ft, f in _folder_listing(folder)])
+        summary = "<br/>\n".join(
+            [
+                f'<a href=".{f[tlink:]}" type="{ft}" target="_blank">{f[tname:]}</a>'
+                for ft, f in _folder_listing(folder)
+            ]
+        )
 
     index_fp = join(out_dir, "summary.html")
-    with open(index_fp, 'w') as of:
+    with open(index_fp, "w") as of:
         of.write(summary)
 
     # we could add a support folder for the summary
@@ -69,22 +71,22 @@ def generate_html_summary(qclient, job_id, parameters, out_dir):
     # we are going to use the "raw" code for retrieving artifact_info vs. the
     # qiita_client.artifact_and_preparation_files method because this only
     # expects a single filepath
-    artifact_id = parameters['input_data']
+    artifact_id = parameters["input_data"]
     qclient_url = "/qiita_db/artifacts/%s/" % artifact_id
     artifact_info = qclient.get(qclient_url)
 
     # [0] there is only one directory
-    folder = artifact_info['files']['directory'][0]['filepath']
+    folder = artifact_info["files"]["directory"][0]["filepath"]
 
     # 2. Generate summary
     index_fp, viz_fp = _generate_html_summary(job_id, folder, out_dir)
 
     # Step 3: add the new file to the artifact using REST api
     success = True
-    error_msg = ''
+    error_msg = ""
     try:
-        fps = dumps({'html': index_fp, 'dir': viz_fp})
-        qclient.patch(qclient_url, 'add', '/html_summary/', value=fps)
+        fps = dumps({"html": index_fp, "dir": viz_fp})
+        qclient.patch(qclient_url, "add", "/html_summary/", value=fps)
     except Exception as e:
         success = False
         error_msg = str(e)
--- qtp_job_output_folder/tests/test_summary.py
+++ qtp_job_output_folder/tests/test_summary.py
@@ -6,13 +6,13 @@
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
-from unittest import main
-from tempfile import mkdtemp
-from os import remove
-from os.path import exists, isdir, join, dirname, abspath
 from inspect import currentframe, getfile
-from shutil import rmtree, copytree
 from json import dumps
+from os import remove
+from os.path import abspath, dirname, exists, isdir, join
+from shutil import copytree, rmtree
+from tempfile import mkdtemp
+from unittest import main
 
 from qiita_client.testing import PluginTestCase
 
@@ -23,8 +23,8 @@ from qtp_job_output_folder.summary import generate_html_summary
 class SummaryTests(PluginTestCase):
     def setUp(self):
         self.out_dir = mkdtemp()
-        self.source_dir = join(mkdtemp(), 'test_data')
-        source = join(dirname(abspath(getfile(currentframe()))), 'test_data')
+        self.source_dir = join(mkdtemp(), "test_data")
+        source = join(dirname(abspath(getfile(currentframe()))), "test_data")
         copytree(source, self.source_dir)
         self._clean_up_files = [self.out_dir]
 
@@ -37,21 +37,28 @@ class SummaryTests(PluginTestCase):
                     remove(fp)
 
     def test_summary(self):
-        files = [(self.source_dir, 'directory')]
-        data = {'filepaths': dumps(files), 'type': 'job-output-folder',
-                'name': "A name", 'data_type': 'Job Output Folder'}
-        aid = self.qclient.post('/apitest/artifact/', data=data)['artifact']
-        parameters = {'input_data': aid}
-        data = {'command': dumps(['qtp-job-output-folder', __version__,
-                                  'Generate HTML summary']),
-                'parameters': dumps(parameters),
-                'status': 'running'}
-        job_id = self.qclient.post(
-            '/apitest/processing_job/', data=data)['job']
+        files = [(self.source_dir, "directory")]
+        data = {
+            "filepaths": dumps(files),
+            "type": "job-output-folder",
+            "name": "A name",
+            "data_type": "Job Output Folder",
+        }
+        aid = self.qclient.post("/apitest/artifact/", data=data)["artifact"]
+        parameters = {"input_data": aid}
+        data = {
+            "command": dumps(
+                ["qtp-job-output-folder", __version__, "Generate HTML summary"]
+            ),
+            "parameters": dumps(parameters),
+            "status": "running",
+        }
+        job_id = self.qclient.post("/apitest/processing_job/", data=data)["job"]
 
         # Run the test
         obs_success, obs_ainfo, obs_error = generate_html_summary(
-            self.qclient, job_id, parameters, self.out_dir)
+            self.qclient, job_id, parameters, self.out_dir
+        )
 
         # asserting reply
         self.assertTrue(obs_success)
@@ -61,36 +68,41 @@ class SummaryTests(PluginTestCase):
         # asserting content of html
         res = self.qclient.get("/qiita_db/artifacts/%s/" % aid)
         # cleaning artifact files, to avoid errors
-        [self._clean_up_files.extend([ff['filepath']])
-         for f in res['files'].values() for ff in f]
-        html_fp = res['files']['html_summary'][0]['filepath']
+        [
+            self._clean_up_files.extend([ff["filepath"]])
+            for f in res["files"].values()
+            for ff in f
+        ]
+        html_fp = res["files"]["html_summary"][0]["filepath"]
         with open(html_fp) as html_f:
             html = html_f.read()
 
         self.assertCountEqual(
-            sorted(html.replace('<br/>', '').split('\n')),
-            sorted(EXP_HTML.format(aid=aid).replace('<br/>', '').split('\n')))
+            sorted(html.replace("<br/>", "").split("\n")),
+            sorted(EXP_HTML.format(aid=aid).replace("<br/>", "").split("\n")),
+        )
 
 
 EXP_HTML = (
     '<a href="./{aid}/test_data/folder_a" type="folder" target="_blank">'
-    'test_data/folder_a</a><br/>\n'
+    "test_data/folder_a</a><br/>\n"
     '<a href="./{aid}/test_data/folder_a/folder_b/folder_c" type="folder" '
     'target="_blank">test_data/folder_a/folder_b/folder_c</a><br/>\n'
     '<a href="./{aid}/test_data/file_2" type="file" target="_blank">'
-    'test_data/file_2</a><br/>\n'
+    "test_data/file_2</a><br/>\n"
     '<a href="./{aid}/test_data/file_1" type="file" target="_blank">'
-    'test_data/file_1</a><br/>\n'
+    "test_data/file_1</a><br/>\n"
     '<a href="./{aid}/test_data/test_data" type="folder" target="_blank">'
-    'test_data/test_data</a><br/>\n'
+    "test_data/test_data</a><br/>\n"
     '<a href="./{aid}/test_data/test_data/folder_a/folder_b" type="folder" '
     'target="_blank">test_data/test_data/folder_a/folder_b</a><br/>\n'
     '<a href="./{aid}/test_data/test_data/folder_a/folder_b/folder_c/file_c" '
     'type="file" target="_blank">test_data/test_data/folder_a/folder_b/'
-    'folder_c/file_c</a><br/>\n'
+    "folder_c/file_c</a><br/>\n"
     '<a href="./{aid}/test_data/test_data/folder_a/file_a" type="file" '
-    'target="_blank">test_data/test_data/folder_a/file_a</a>')
+    'target="_blank">test_data/test_data/folder_a/file_a</a>'
+)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
