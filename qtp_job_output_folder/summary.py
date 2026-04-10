42b510859576dfba2fd37809015d644aa03cf4c7
Stefan Janssen <stefan.m.janssen@gmail.com>
2026-04-11 00:00:01 +0200

    write debug into file

--- qtp_job_output_folder/summary.py
+++ qtp_job_output_folder/summary.py
@@ -9,9 +9,7 @@
 from json import dumps
 from os import sep, walk
 from os.path import basename, dirname, exists, isdir, join
-import logging
-
-logger = logging.getLogger(__name__)
+from glob import glob
 
 def _folder_listing(folder):
     index, manifest = [], []
@@ -37,7 +35,7 @@ def _folder_listing(folder):
         manifest.append(f"{space} {basename(dpath)}/")
         for filename in files:
             manifest.append(f"{space}{separator} {filename}")
-    logger.debug("_folder_listing(folder=%s): index=%s, manifest=%s" % (folder, index, manifest))
+
     return index, manifest
 
 
@@ -53,7 +51,8 @@ def _generate_html_summary(jid, folder, out_dir):
         link = '<a href=".%s" type="%s" target="_blank">%s</a>'
         index, manifest = _folder_listing(folder)
 
-        logger.debug("\nSTEFAN summary: write manifest to file %s\n" % manifest_fp)
+        with open("/tmp/stefan.log", "a") as f:
+            f.write("_generate_html_summary, pre write manifest, manifest_fp=%s, manifest=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (manifest_fp, manifest, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
         with open(manifest_fp, "w") as of:
             of.write("\n".join(manifest))
 
@@ -68,7 +67,8 @@ def _generate_html_summary(jid, folder, out_dir):
 
     # we could add a support folder for the summary
     viz_fp = None
-    logger.debug("_generate_html_summary(jid, folder=%s, out_dir=%s) index_fp=%s, viz_fp=%s\n" % (folder, out_dir, index_fp, viz_fp))
+    with open("/tmp/stefan.log", "a") as f:
+        f.write("_generate_html_summary, pre return, index_fp=%s, viz_fp=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (index_fp, viz_fp, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
     return index_fp, viz_fp
 
 
@@ -99,21 +99,32 @@ def generate_html_summary(qclient, job_id, parameters, out_dir):
     # expects a single filepath
     artifact_id = parameters["input_data"]
     qclient_url = "/qiita_db/artifacts/%s/" % artifact_id
+    with open("/tmp/stefan.log", "a") as f:
+        f.write("generate_html_summary, pre qclient.get(), artifact_id=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (artifact_id, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
     artifact_info = qclient.get(qclient_url)
-
+    with open("/tmp/stefan.log", "a") as f:
+        f.write("generate_html_summary, post qclient.get(), artifact_id=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (artifact_id, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
+    
     # [0] there is only one directory
     folder = artifact_info["files"]["directory"][0]["filepath"]
 
     # 2. Generate summary
+    with open("/tmp/stefan.log", "a") as f:
+        f.write("generate_html_summary, pre _generate_html_summary(job_id=%s, folder=%s, out_dir=%s), glob@tmp=%s, glob@qiita-data=%s, glob@folder=%s\n" % (job_id, folder, out_dir, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True)), '\n'.join(glob(folder+"/**/*", recursive=True))))
     index_fp, viz_fp = _generate_html_summary(job_id, folder, out_dir)
-
+    with open("/tmp/stefan.log", "a") as f:
+        f.write("generate_html_summary, post _generate_html_summary, index_fp=%s, viz_fp=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (index_fp, viz_fp, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
+    
     # Step 3: add the new file to the artifact using REST api
     success = True
     error_msg = ""
     try:
         fps = dumps({"html": index_fp, "dir": viz_fp})
-        logger.debug("STEFAN voir patch: value (type=%s)=%s" % (type(fps), fps))
+        with open("/tmp/stefan.log", "a") as f:
+            f.write("generate_html_summary, pre patch(fps=%s), glob@tmp=%s, glob@qiita-data=%s\n" % (fps, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
         qclient.patch(qclient_url, "add", "/html_summary/", value=fps)
+        with open("/tmp/stefan.log", "a") as f:
+            f.write("generate_html_summary, post patch(fps=%s), glob@tmp=%s, glob@qiita-data=%s\n" % (fps, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
     except Exception as e:
         success = False
         error_msg = str(e)
