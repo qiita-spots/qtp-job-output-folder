796b5b694d24a6e08eba7df3c79a5ee622db465d
Stefan Janssen <stefan.m.janssen@gmail.com>
2026-04-10 12:02:06 +0200

    also debug print manifest

--- qtp_job_output_folder/tests/test_summary.py
+++ qtp_job_output_folder/tests/test_summary.py
@@ -99,17 +99,19 @@ class SummaryTests(PluginTestCase):
         self.maxDiff = None
         import sys
         print("html: >%s<\n\nEXP_HTML: >%s<" % (html, EXP_HTML), file=sys.stderr)
-        # check list of entries, as order might differ
-        self.assertCountEqual(
-            html.split('<br/>\n'),
-            EXP_HTML.format(aid=aid, dir=self.exp_dirname).split('<br/>\n')
-        )
 
         # verifying the new MANIFEST.txt
         mfp = join(res["files"]["directory"][0]["filepath"], "MANIFEST.txt")
         self.assertTrue(exists(f"{mfp}"))
         with open(mfp, "r") as f:
             obs = f.readlines()
+        # check list of entries, as order might differ
+        print("manifest\nobs=>%s<\nexp=>%s<\n" % ('\n'.join(obs), '\n'.join(EXP_MANIFEST)))
+
+        self.assertCountEqual(
+            html.split('<br/>\n'),
+            EXP_HTML.format(aid=aid, dir=self.exp_dirname).split('<br/>\n')
+        )
         self.assertCountEqual(
             list(map(str.strip, obs)),
             list(map(
