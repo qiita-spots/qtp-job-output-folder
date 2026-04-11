386aa047c6f9603f3e3a387828fd7e3d065fdec6
Stefan Janssen <stefan.m.janssen@gmail.com>
2026-04-11 12:15:05 +0200

    change debug path

--- qtp_job_output_folder/summary.py
+++ qtp_job_output_folder/summary.py
@@ -51,7 +51,7 @@ def _generate_html_summary(jid, folder, out_dir):
         link = '<a href=".%s" type="%s" target="_blank">%s</a>'
         index, manifest = _folder_listing(folder)
 
-        with open("/tmp/stefan.log", "a") as f:
+        with open("/debug/stefan.log", "a") as f:
             f.write("_generate_html_summary, pre write manifest, manifest_fp=%s, manifest=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (manifest_fp, manifest, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
         with open(manifest_fp, "w") as of:
             of.write("\n".join(manifest))
@@ -67,7 +67,7 @@ def _generate_html_summary(jid, folder, out_dir):
 
     # we could add a support folder for the summary
     viz_fp = None
-    with open("/tmp/stefan.log", "a") as f:
+    with open("/debug/stefan.log", "a") as f:
         f.write("_generate_html_summary, pre return, index_fp=%s, viz_fp=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (index_fp, viz_fp, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
     return index_fp, viz_fp
 
@@ -99,20 +99,20 @@ def generate_html_summary(qclient, job_id, parameters, out_dir):
     # expects a single filepath
     artifact_id = parameters["input_data"]
     qclient_url = "/qiita_db/artifacts/%s/" % artifact_id
-    with open("/tmp/stefan.log", "a") as f:
+    with open("/debug/stefan.log", "a") as f:
         f.write("generate_html_summary, pre qclient.get(), artifact_id=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (artifact_id, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
     artifact_info = qclient.get(qclient_url)
-    with open("/tmp/stefan.log", "a") as f:
+    with open("/debug/stefan.log", "a") as f:
         f.write("generate_html_summary, post qclient.get(), artifact_id=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (artifact_id, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
     
     # [0] there is only one directory
     folder = artifact_info["files"]["directory"][0]["filepath"]
 
     # 2. Generate summary
-    with open("/tmp/stefan.log", "a") as f:
+    with open("/debug/stefan.log", "a") as f:
         f.write("generate_html_summary, pre _generate_html_summary(job_id=%s, folder=%s, out_dir=%s), glob@tmp=%s, glob@qiita-data=%s, glob@folder=%s\n" % (job_id, folder, out_dir, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True)), '\n'.join(glob(folder+"/**/*", recursive=True))))
     index_fp, viz_fp = _generate_html_summary(job_id, folder, out_dir)
-    with open("/tmp/stefan.log", "a") as f:
+    with open("/debug/stefan.log", "a") as f:
         f.write("generate_html_summary, post _generate_html_summary, index_fp=%s, viz_fp=%s, glob@tmp=%s, glob@qiita-data=%s\n" % (index_fp, viz_fp, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
     
     # Step 3: add the new file to the artifact using REST api
@@ -120,10 +120,10 @@ def generate_html_summary(qclient, job_id, parameters, out_dir):
     error_msg = ""
     try:
         fps = dumps({"html": index_fp, "dir": viz_fp})
-        with open("/tmp/stefan.log", "a") as f:
+        with open("/debug/stefan.log", "a") as f:
             f.write("generate_html_summary, pre patch(fps=%s), glob@tmp=%s, glob@qiita-data=%s\n" % (fps, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
         qclient.patch(qclient_url, "add", "/html_summary/", value=fps)
-        with open("/tmp/stefan.log", "a") as f:
+        with open("/debug/stefan.log", "a") as f:
             f.write("generate_html_summary, post patch(fps=%s), glob@tmp=%s, glob@qiita-data=%s\n" % (fps, '\n'.join(glob("/tmp/**/*", recursive=True)), '\n'.join(glob("/qiita_data/**/*", recursive=True))))
     except Exception as e:
         success = False
